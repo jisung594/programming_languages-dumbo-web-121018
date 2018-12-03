6b991b4f26b81834d1c59b442d9d2108a173bd90
jonathan choi <jisung594@gmail.com>
2018-12-03 00:14:10 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  
+  
+  languages.each do |styles,data1|
+    data1.each do |lang, data2|
+      new_hash[:lang] = data2
+      binding.pry
+    end
+  end
+  
+  return new_hash
 end
+
+
+#data2 is a hash with 'type'(key) and info(value)
+
+
+=begin
+{
+  :ruby => {      #lang will be key,a hash containing info will be val
+    :type => "interpreted",
+    :style => [:oo]
+  },
+  :javascript => {
+    :type => "interpreted",
+    :style => [:oo, :functional]
+  }
+}
