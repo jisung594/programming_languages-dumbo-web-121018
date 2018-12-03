313c011a5577c0f6732386879ddd9a770a136faa
jonathan choi <jisung594@gmail.com>
2018-12-03 14:39:52 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  
+  languages.each do |style,lang|
+    lang.each do |name,type_info|
+      new_hash[name] = type_info
+      new_hash[name][:style] << style
+      binding.pry
+    end
+  end
+  
+  return new_hash
 end
+
